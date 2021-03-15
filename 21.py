e1236b8a4a3eb52bfc5906979a2bd38a893f0d95
guaxez <vricardimelo@gmail.com>
2021-03-15 14:06:55 -0300

    Carta add naipes e tipos

--- 21.py
+++ 21.py
@@ -4,12 +4,33 @@ Jogo de 21
 
 class Carta():
 
+    tipos = {
+        '1':'Ás',
+        '2':'Dois',
+        '3':'Três',
+        '4':'Quatro',
+        '5':'Cinco',
+        '6':'Seis',
+        '7':'Sete',
+        '8':'Oito',
+        '9':'Nove',
+        '10':'Dez',
+        '11':'Valete',
+        '12':'Dama',
+        '13':'Rei',
+        'O':'Ouros',
+        'E':'Espadas',
+        'C':'Copas',
+        'P':'Paus'
+    }
+
+
     def __init__(self, num, tipo):
-        self.numero = num
-        self.naipe = tipo
+        self.numero = self.tipos[str(num)]
+        self.naipe = self.tipos[str(tipo)]
 
     def __str__(self):
-        return '{}{}'.format(self.numero,self.naipe)
+        return '{} de {}'.format(self.numero,self.naipe)
 
 
 print('21')
